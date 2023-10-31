e792996afb972a2bc37795a2984c892dd011bf0f
RaphaelC1 <100593405+RaphaelC1@users.noreply.github.com>
2023-10-31 15:59:24 +0100

    server.js qui marche

---
+++ WEBAPP/controllers/hello.route.js
@@ -0,0 +1,29 @@
+// controllers/hello.route.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/my/:name', mynameAction);
+router.get('/myy', mynameAction);
+async function mynameAction(request, response) {
+    response.send("MYNAME ACTION "+request.params.name);
+}
+
+
+// http://localhost:9000/hello
+router.get('/', (req, res) => {
+    //res.send('Hello, from controller...');
+    res.render('hello_view', { favourites: [] });
+});
+
+// http://localhost:9000/hello/world
+router.get('/world', (req, res) => {
+    //res.send('Hello WORLD, from controller...');
+    res.render('hello_view', { favourites: [
+        { category: 'cheese', thing: 'raclette' },
+        { category: 'book', thing: 'Umberto Eco: Foucaults pendulum' },
+        { category: 'drink', thing: 'cidre brut' },
+        { category: 'color', thing: 'green (allez les Verts! ;) ' },
+    ] });
+});
+
+module.exports = router;
--- WEBAPP/server.js
+++ WEBAPP/server.js
@@ -17,35 +17,7 @@ app.get('/', (request, response) => { // 'GET' as a HTTP VERB, not as a 'getter'
 
 // MIDDLEWARE REGISTRATIONS
 // app.use(callback1, callback2, callback3)
-const bodyParser = require("body-parser");
-app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
-
-const session = require("express-session");
-app.use(session({
-    secret: "SecretRandomStringDskghadslkghdlkghdghaksdghdksh",
-    saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day in msec
-    resave: false
-}));
-
-//if (request.session.cart===undefined) request.session.cart = [];
-//request.session.cart.push("xxxx");
-
-const auth = require("./utils/users.auth");
-auth.initialization(app);
 
 // app.use(routeBase, callback);
 app.use("/static", express.static(__dirname + '/static'));
 app.use("/hello", require("./controllers/hello.route"));
-app.use("/cars", require("./controllers/cars.route"));
-app.use("/auth", require("./controllers/auth.route"));
-
-// npm install cors
-var cors = require('cors');
-const { request } = require('express');
-app.use(cors());
-app.use("/carsapi", require("./controllers/carsapi.route"));
-
-
-let value = "something";
-let other = `xxx yyy ${value} zzz`;
